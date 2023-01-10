cda2400f2b1afca026f10247e72f52ae2045fe7d
Boris Rozov work <outsource@list.ru>
2023-01-10 16:38:19 +0300

    Initial commit

---
+++ src/main/java/org/example/MainClient.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+public class MainClient {
+    public static void main (String[] args) throws IOException {
+        String host = "netology.homework";
+        int port = 8080;
+        String name = "Bob";
+        String kid = "no";
+
+        try(Socket clientSocket = new Socket(host, port);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            System.out.println(in.readLine());
+            out.println(name);
+            System.out.println(name);
+            System.out.println(in.readLine());
+            out.println(kid);
+            System.out.println(kid);
+            System.out.println(in.readLine());
+            System.out.println("closing...");
+        }
+    }
+}
---
+++ src/main/java/org/example/MainServer.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class MainServer {
+    public static void main (String[] args) throws IOException {
+        int port = 8080;
+        // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый -
+        // рекомендуем использовать около 8080
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Wait connection on " + serverSocket);
+        try {
+            Socket clientSocket = serverSocket.accept(); // ждем подключения
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("New connection accepted");
+            out.println("Write your name");
+            String name = in.readLine();
+            System.out.println("Client name: " + name);
+            out.println(name + " are you child? (yes/no)");
+            String adult = in.readLine();
+            if (adult.equals("yes")) {
+                System.out.println(name + " is kid.");
+                out.println("Welcome to the kids area, " + name + "! Let's play!");
+            } else if (adult.equals("no")) {
+                System.out.println(name + " is adult.");
+                out.println("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!");
+            } else {
+                out.println("I don't no your kid or adult, good bye!");
+            }
+            clientSocket.close();
+        } finally {
+            System.out.println("closing...");
+            serverSocket.close();
+        }
+    }
+}
